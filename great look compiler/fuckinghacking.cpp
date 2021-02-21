3f8fa79687d8e2f4926c6840ed75d03573fcabf6
cerona1423 <theflyruler@gmail.com>
2021-02-21 13:05:25 +0900

    first commit

---
+++ great look compiler/fuckinghacking.cpp
@@ -0,0 +1,25 @@
+// https://youtu.be/DwwDDUJ4uFk
+#include <iostream>
+#include <cstdlib>
+
+int color_rand();
+
+int main()
+{
+    system("color a");
+    printf("tester");
+    system("timeout 2");
+    system("tree c:\\");
+    system("pause");
+    printf("now mad [] dam won");
+    
+    return 0;
+}
+
+int color_rand()
+{
+    int color_code[] = {1,2,3,4,5,6,7,8,9};
+    int i;
+    i = rand(0,8);
+    return 0;
+}
